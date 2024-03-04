451a2628b490bfdd1b0016c450cf48c7d1414c95
deathperminut <mendezsebas8@gmail.com>
2024-03-04 06:49:09 -0500

    "Creamos logica de servicios queda faltando la conexión con el front"

---
+++ src/Services/Auth/Auth.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { environment } from "../../environments/environments";
+
+
+const RegisterUser=async (body)=>{
+    const path  = environment.api + environment.register;
+
+    return await axios.post(path,body);
+}
+
+
+const loginUser= async (body)=>{
+    const path =  environment.api + environment.login;
+
+    return await axios.post(path,body);
+}
+
+const resetPassword = async (body)=>{
+    const path = environment.api + environment.resetPassword;
+    return await axios.post(path,body);
+}
+
+const confirmResetPassword =  async (body)=>{
+    const path =  environment.api +  environment.completeReset;
+    return await axios.post(path,body);
+}
+
+export {RegisterUser,loginUser,resetPassword,confirmResetPassword}
