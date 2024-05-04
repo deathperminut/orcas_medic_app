d3baa95d1a7f55b686ca563392b54a04c93bd1ed
Sebastian Mendez <mendezsebas8@gmail.com>
2024-05-04 16:01:49 -0500

    "Empezamos a crear pestaña para las estadisticas del usuario"

---
+++ src/MedicApp/Patient/ModulsPatient/HistoryDates/StadisticsPatient/StadisticsPatient.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './StadisticsPatient.css';
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'malihu-custom-scrollbar-plugin';
+import { AppContext } from '../../../../../context';
+import Swal from 'sweetalert2';
+import Preloader from '../../../../../components/Preloader/Preloader';
+import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css';
+import { FaEye } from "react-icons/fa";
+
+
+export default function StadisticsPatient() {
+    return (
+        <div>StadisticsPatient</div>
+    )
+}
--- src/context/index.js
+++ src/context/index.js
@@ -11,11 +11,12 @@ function ProviderContext(props){
     let [token,setToken] = React.useState(null);
     let [typeDate,setTypeDate] = React.useState(null);
     let [dni,setDni] = React.useState("");
+    let [selectCompleteDate,setSelectCompleteDate] = React.useState(null);
     
 
     return (
         
-        <AppContext.Provider value={{dni,setDni,token,setToken,userData,setUserData,typeDate,setTypeDate}}>
+        <AppContext.Provider value={{dni,setDni,token,setToken,userData,setUserData,typeDate,setTypeDate,selectCompleteDate,setSelectCompleteDate}}>
             {props.children}
         </AppContext.Provider>
         
