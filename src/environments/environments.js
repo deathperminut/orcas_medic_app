14700725622692a8ae7f3865c17e6fb33a7eb4fd
Sebastian Mendez <mendezsebas8@gmail.com>
2024-05-13 20:54:44 -0500

    "Ubicamos los endpoints en el environment"

--- src/environments/environments.js
+++ src/environments/environments.js
@@ -32,9 +32,59 @@ export const environment = {
     'test_ansiedad':'indicadores/retornar-test-ansiedad/',
     'test_depresion':'indicadores/retornar-test-depresion/',
     'test_estres':'indicadores/retornar-test-estres/',
-
+    
+    /*******************/
     // HISTORIA CLINICA
+    /******************/
 
-    create_antescedentes:''
+    // AGENDAMIENTO
+    
+    'crear_agendamiento':'agendamiento/crear-agendamiento/',
+
+    // CITAS
+
+    'crear_cita':'historia-clinica/crear-cita/',
+    'actualizar_cita':'historia-clinica/actualizar-cita/',
+
+
+    // ANTESCEDENTES
+    
+    'crear_antescedentes':'historia-clinica/crear-antecedente/',
+    'actualizar_antescendentes':'historia-clinica/actualizar-antecedente/',
+    'obtener_ultimo_antescendentes':'historia-clinica/retornar-ultimo-antecedente/',
+    
+    // REGISTROS ESPECIFICOS
+
+    'crear_registros':'historia-clinica/crear-registros-especificos/',
+    'actualizar_registros':'historia-clinica/actualizar-registros-especificos/',
+    'obtener_ultimo_registro':'historia-clinica/retornar-ultimo-registros-especificos/',
+
+    // TEST GENERAL
+
+    'crear_test_general':'historia-clinica/crear-test-general/',
+    'actualizar_test_general':'historia-clinica/actualizar-test-general/',
+    'obtener_ultimo_test_general':'historia-clinica/retornar-ultimo-test-general/',
+
+    // TEST ANSIEDAD
+
+    'crear_test_ansiedad':'historia-clinica/crear-test-ansiedad/',
+    'actualizar_test_ansiedad':'historia-clinica/actualizar-test-ansiedad/',
+    'obtener_ultimo_test_ansiedad':'historia-clinica/retornar-ultimo-test-ansiedad/',
+
+    // TEST DEPRESIÓN
+
+    'crear_test_depresion':'historia-clinica/crear-test-depresion/',
+    'actualizar_test_depresion':'historia-clinica/actualizar-test-depresion/',
+    'obtener_ultimo_test_depresion':'historia-clinica/retornar-ultimo-test-depresion/',
+
+    // TEST ESTRES
+
+    'crear_test_estres':'historia-clinica/crear-test-estres/',
+    'actualizar_test_estres':'historia-clinica/actualizar-test-estres/',
+    'obtener_ultimo_test_estres':'historia-clinica/retornar-ultimo-test-estres/'
+
+
+
+    
 
 }
