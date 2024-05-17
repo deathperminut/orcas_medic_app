bfc7ea75b330a77dd7e78be9a699f4ff01d12be3
Sebastian Mendez <mendezsebas8@gmail.com>
2024-05-17 00:05:22 -0500

    "Generamos componentes de encuestas"

---
+++ src/MedicApp/Medic/ModulsMedic/Date/MedicHistory_4/MedicHistory_4.jsx
@@ -0,0 +1,365 @@
+import React from 'react'
+import './MedicHistory_2.css'
+import Select, { components } from 'react-select'
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
+import DatePicker from "react-multi-date-picker";
+import TimePicker from "react-multi-date-picker/plugins/time_picker";
+import "react-tooltip/dist/react-tooltip.css";
+
+import makeAnimated from 'react-select/animated';
+import { AppContext } from '../../../../../context';
+/* MEDICAL HISTORY COMPONENTS */
+
+import EegData from '../MedicHistory/HistoryComponents/EegData/EegData';
+/**
+ * MENSAJES PERSONALIZADOS AL BUSCAR O CARGAR OPCIONES EN REACT SELECT
+ */
+
+const { NoOptionsMessage } = components;
+
+const customNoOptionsMessage = props => (
+  <NoOptionsMessage {...props} className="custom-no-options-message-internal-form-">No registrado</NoOptionsMessage>
+);
+
+const { LoadingMessage } = components;
+
+const customLoadingMessage = props => (
+  <LoadingMessage {...props} className="custom-loading-message-internal-form-">Cargando</LoadingMessage>
+);
+
+/**
+ * ANIMATE DELETE MULTISELECT
+ */
+
+const animatedComponents = makeAnimated();
+
+/**
+ * Data que llena los select
+ */
+
+const Parentage = [
+  { value: "opcion-uno", label: "Opcion uno" },
+  { value: "opcion-dos", label: "Opcion dos" },
+  { value: "opcion-tres", label: "Opcion tres" }
+];
+
+const MaritalStatus = [
+  { value: "opcion-uno", label: "Opcion uno" },
+  { value: "opcion-dos", label: "Opcion dos" },
+  { value: "opcion-tres", label: "Opcion tres" }
+];
+
+const PopulationGroup  = [
+  { value: "opcion-uno", label: "Opcion uno" },
+  { value: "opcion-dos", label: "Opcion dos" },
+  { value: "opcion-tres", label: "Opcion tres" }
+];
+
+const EthnicGroup  = [
+  { value: "opcion-uno", label: "Opcion uno" },
+  { value: "opcion-dos", label: "Opcion dos" },
+  { value: "opcion-tres", label: "Opcion tres" }
+];
+
+const Religion  = [
+  { value: "opcion-uno", label: "Opcion uno" },
+  { value: "opcion-dos", label: "Opcion dos" },
+  { value: "opcion-tres", label: "Opcion tres" }
+];
+
+/**
+ * Se genera componente nuevo para soportar el placeholder animado del input 
+ */
+
+const { ValueContainer, Placeholder } = components;
+
+const CustomValueContainer = ({ children, ...props }) => {
+  const { inputId, placeholder } = props.selectProps;
+  return (
+    <ValueContainer {...props}>
+      <Placeholder htmlFor={inputId} {...props}>
+        {placeholder}
+      </Placeholder>
+      {React.Children.map(children, child =>
+        child && child.type !== Placeholder ? child : null
+      )}
+    </ValueContainer>
+  );
+};
+
+/**
+* Constante que soporta todo el cambio de los estilo del select
+*/
+
+const selectStyles = {
+  /**
+  * Estilos del icono del dropdown del select
+  * Estilos del separador del select
+  * Estilos del icono de cerrar del select
+  */
+  dropdownIndicator: (styles) => ({ ...styles, 
+    color: "#d1a207", 
+    padding: 0, paddingTop: '0.34rem !important', 
+    paddingRight: '0.5rem !important',
+    width: '25px',
+    height: '25px',
+    "&:hover": {
+      color: "var(--color-black-)",
+    } 
+  }),
+  indicatorSeparator: (styles) => ({ ...styles, display: "none" }),
+  clearIndicator: (styles) => ({ ...styles, 
+    color: "white", 
+    padding: 0, 
+    paddingTop: '0.05rem !important',
+    width: '15px',
+    height: '15px',
+    "&:hover": {
+      color: "white",
+    } 
+  }),
+  /**
+  * Estilos del input global
+  */
+  control: () => ({
+  fontSize: 14,
+  display: "flex",
+  alignItems: "center",
+  alignSelf: "start",
+  justifyContent: "start",
+  height: 'auto',
+  minHeight: 50,
+  maxHeight: 150,
+  paddingLeft: '2.1rem',
+  paddingTop: '0.3rem',
+  width: "100%",
+  backgroundColor: 'transparent',
+  borderRadius: 0,
+  borderBottom: "1px solid #d1a207",
+  }),
+  /**
+  * EESTILOS DEL INPUT
+  */
+  input: (provided) => ({
+  ...provided,
+  color: 'var(--color-quaternary-gray-)',
+  fontSize: 12,
+  textTransform: 'uppercase',
+  fontFamily: 'var(--font-family-regular-)',
+  }),
+  /**
+  * Estilos del menu desplegable del select
+  */
+  menu: (styles) => ({
+  ...styles,
+  border: 'none',
+  backgroundColor: 'var(--color-secondary-white-rgba-)',
+  boxShadow: 'var(--box-shadow-2-)',
+  borderRadius: '1rem',
+  padding: 0,
+  marginTop: 8,
+  marginBottom: 0,
+  height: 'auto',
+  minHeight: 'auto',
+  maxHeight: 300,
+  overflow: "hidden",
+  color: 'var(--color-quaternary-gray-)',
+  fontSize: 12,
+  textTransform: 'uppercase',
+  fontFamily: 'var(--font-family-regular-)',
+  }),
+  menuList: () => ({
+    paddingTop: 0,
+    paddingBottom: 0,
+    height: 'auto',
+    minHeight: 'auto',
+    maxHeight: 300,
+    overflow: "auto",
+    "::-webkit-scrollbar": {
+      width: "0px !important",
+      height: "0px !important",
+    },
+    "::-webkit-scrollbar-track": {
+      background: "transparent !important"
+    },
+    "::-webkit-scrollbar-thumb": {
+      background: "transparent !important"
+    },
+    "::-webkit-scrollbar-thumb:hover": {
+      background: "transparent !important"
+    }
+  }),
+  /**
+  * Estilos de las opciones desplegables
+  */
+  option: (provided, state) => ({
+  ...provided,
+  fontSize: 11,
+  textTransform: 'uppercase',
+  backgroundColor: state.isSelected ? "var(--color-purple-)" : "var(--color-secondary-white-rgba-)",
+  fontFamily: 'var(--font-family-regular-)',
+  padding: '0.5rem 0.8rem 0.5rem 0.8rem',
+  borderRadius: '1rem',
+  ":hover": {
+  background: "var(--color-purple-)",
+  color: 'var(--color-white-)',
+  }
+  }),
+  /**
+  * Estilos del contenedor
+  */
+  container: (provided, state) => ({
+  ...provided,
+  marginTop: 0,
+  width: '100%',
+  position: 'relative',
+  flex: '1 1 auto'
+  }),
+  valueContainer: (provided, state) => ({
+  ...provided,
+  overflow: "visible"
+  }),
+  /**
+  * Estilos placeholder del input
+  */
+  placeholder: (provided, state) => ({
+  ...provided,
+  width: '100%',
+  position: "absolute",
+  top: state.hasValue || state.selectProps.inputValue ? -15 : "28%",
+  left: state.hasValue || state.selectProps.inputValue ? -32 : "0%",
+  transition: "top 0.1s, font-size 0.1s",
+  color: 'var(--color-quaternary-gray-)',
+  fontSize: state.hasValue || state.selectProps.inputValue ? 13 : "14px",
+  lineHeight: 1.25,
+  fontFamily: 'var(--font-family-regular-)',
+  overflow: 'hidden',
+  textAlign: 'start',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap'
+  }),
+  /**
+  * Estilos texto en el input
+  */
+  singleValue: (styles) => ({ 
+  ...styles, 
+  fontSize: 12,
+  textTransform: 'uppercase',
+  color: "var(--color-quaternary-gray-)", 
+  fontFamily: 'var(--font-family-regular-)', 
+  padding: '3px',
+  margin: '0px',
+  marginTop: '7px',
+  marginLeft: 0,
+  marginRight: 0
+  }),
+  multiValue: (styles) => ({ 
+    ...styles, 
+    backgroundColor: 'var(--color-secondary-white-rgba-)',
+    boxShadow: 'var(--box-shadow-2-)',
+    borderRadius: '1rem',
+    margin: '2px',
+    alignItems: 'center',
+    alignSelf: 'center',
+  }),
+  multiValueLabel: (styles, { data }) => ({
+    ...styles,
+    fontFamily: 'var(--font-family-regular-)',
+    fontSize: 12,
+    textTransform: 'uppercase',
+    color: 'var(--color-quaternary-gray-)',
+    borderRadius: '1rem',
+    padding: '5px',
+    paddingLeft: '0.5rem',
+    paddingRight: '0.6rem',
+    paddingBottom: '0.3rem'
+  }),
+  multiValueRemove: (styles, { data }) => ({
+    ...styles,
+    borderRadius: '6rem',
+    paddingLeft: '6px',
+    width: '26px',
+    height: '26px',
+    color: 'var(--color-black-)',
+    backgroundColor: 'var(--color-secondary-gray-)',
+    ':hover': {
+      color: 'var(--color-white-)',
+      backgroundColor: 'var(--color-secondary-purple-)',
+    }
+  })
+}
+
+/**
+ * MESES Y DIAS EN ESPAÑOL PARA EL DATEPICKER
+ */
+
+const months = [
+  ["Ene", "Ene"],
+  ["Feb", "Feb"],
+  ["Mar", "Mar"],
+  ["Abr", "Abr"],
+  ["May", "May"],
+  ["Jun", "Jun"],
+  ["Jul", "Jul"],
+  ["Agos", "Ago"],
+  ["Sep", "Sep"],
+  ["Oct", "Oct"],
+  ["Nov", "Nov"],
+  ["Dic", "Dic"],
+]
+const weekDays = [
+  ["Lun", "Lu"],
+  ["Mar", "Ma"],
+  ["Mie", "Mi"],
+  ["Jue", "Ju"],
+  ["Vie", "Vi"],
+  ["Sab", "Sa"],
+  ["Dom", "Do"],
+]
+
+
+export default function MedicHistory_4() {
+
+  let navigate = useNavigate()
+
+  /* APP CONTEXT */
+
+  let {typeDate,setTypeDate} =  React.useContext(AppContext);
+
+
+  return (
+    <React.Fragment>
+      <div style={{'marginBottom':'100px'}} onClick={()=>{
+        navigate('/ModulsMedic/Date_Medic/MakeHistory')}
+        }  className='ButtonElement'>
+                                <span  className='ButtonText'>Volver</span>
+      </div>
+      <div className='row mt-4 mb-4'>
+        <div className='col-12'>
+          <h2 className='m-0 p-0 lh-sm fs-4- ff-monse-regular- fw-bold tx-dark-purple- white font_medium'>Registro EEG</h2>
+        </div>
+      </div>
+      
+      {/* SEGMENTAMOS POR CITA */} 
+      {typeDate == "Evaluación inicial"  ? 
+      <>
+      <EegData></EegData>
+      </>
+      :
+      <></>
+      }
+
+      {typeDate == "Seguimiento regular"  ? 
+      <>
+      <EegData></EegData>
+      </>
+      :
+      <></>
+      }
+      
+      <div style={{'marginBottom':'50px'}} onClick={()=>navigate('/ModulsMedic/Date_Medic/FinishProcess')}  className='ButtonElement'>
+                                <span  className='ButtonText'>Siguiente</span>
+      </div>
+    </React.Fragment>
+  )
+}
