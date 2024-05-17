44e211b9e95ebab3e668ae198dfb9517eea1eb62
Sebastian Mendez <mendezsebas8@gmail.com>
2024-05-16 22:48:20 -0500

    "Terminamos componente de alertas"

--- src/MedicApp/Medic/ModulsMedic/Date/MedicHistory/HistoryComponents/Questionaires/ResultsTest/ResultsTest.jsx
+++ src/MedicApp/Medic/ModulsMedic/Date/MedicHistory/HistoryComponents/Questionaires/ResultsTest/ResultsTest.jsx
@@ -62,6 +62,141 @@ export default function ResultsTest(props) {
         :
         <></>
         }
+
+        {props.type == "Ansiedad" ? 
+        <>
+        {props.result <= 45 ? 
+        <>
+        <GrStatusGood color='#d1a207' size={55}></GrStatusGood>
+        <span className='orangev2 font_medium text-align'>Test de ansiedad</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Sin ansiedad o ansiedad leve.
+        </p>
+        </>
+        :
+        <>
+        {props.result >= 46 && props.result <=65 ? 
+        <>
+        <IoAlertCircleOutline color='#d1a207' size={55}></IoAlertCircleOutline>
+        <span className='orangev2 font_medium text-align'>Test de ansiedad</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Ansiedad moderada.
+        </p>
+        </>
+        :
+        <>
+        {props.result >= 66 && props.result <=90 ? 
+        <>
+        <IoAlertCircleOutline color='#d1a207' size={55}></IoAlertCircleOutline>
+        <span className='orangev2 font_medium text-align'>Test de ansiedad</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Con ansiedad.
+        </p>
+        </>
+        :
+        <>
+        </>
+        }
+        </>
+        }
+        </>
+        }
+        
+        </>
+        :
+        <></>
+        }
+
+        {props.type == "Depresion" ? 
+        <>
+        {props.result <= 45 ? 
+        <>
+        <GrStatusGood color='#d1a207' size={55}></GrStatusGood>
+        <span className='orangev2 font_medium text-align'>Test de depresión</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Sin depresión o depresión leve.
+        </p>
+        </>
+        :
+        <>
+        {props.result >= 46 && props.result <=65 ? 
+        <>
+        <IoAlertCircleOutline color='#d1a207' size={55}></IoAlertCircleOutline>
+        <span className='orangev2 font_medium text-align'>Test de depresión</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Depresión moderada.
+        </p>
+        </>
+        :
+        <>
+        {props.result >= 66 && props.result <=90 ? 
+        <>
+        <IoAlertCircleOutline color='#d1a207' size={55}></IoAlertCircleOutline>
+        <span className='orangev2 font_medium text-align'>Test de depresión</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Depresión grave.
+        </p>
+        </>
+        :
+        <>
+        </>
+        }
+        </>
+        }
+        </>
+        }
+        
+        </>
+        :
+        <></>
+        }
+
+
+        {props.type == "Estres" ? 
+        <>
+        {props.result <= 45 ? 
+        <>
+        <GrStatusGood color='#d1a207' size={55}></GrStatusGood>
+        <span className='orangev2 font_medium text-align'>Test de estrés</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Sin estrés o estrés leve.
+        </p>
+        </>
+        :
+        <>
+        {props.result >= 46 && props.result <=65 ? 
+        <>
+        <IoAlertCircleOutline color='#d1a207' size={55}></IoAlertCircleOutline>
+        <span className='orangev2 font_medium text-align'>Test de depresión</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Estrés moderada.
+        </p>
+        </>
+        :
+        <>
+        {props.result >= 66 && props.result <=90 ? 
+        <>
+        <IoAlertCircleOutline color='#d1a207' size={55}></IoAlertCircleOutline>
+        <span className='orangev2 font_medium text-align'>Test de depresión</span>
+        <p className='white font_medium text-align' style={{'marginTop':'10px'}}>
+        Estrés grave.
+        </p>
+        </>
+        :
+        <>
+        </>
+        }
+        </>
+        }
+        </>
+        }
+        
+        </>
+        :
+        <></>
+        }
+
+
         
     </div>
   )
